6180316b12d6e91ad5235b8d1962e7903c889502
tomsq <finixit@email.cz>
2024-03-16 13:28:15 +0100

    example fix

--- packages/vue3/example/Example.vue
+++ packages/vue3/example/Example.vue
@@ -48,11 +48,11 @@ const calypsoTheme: ZaptimeConfig['theme'] = {
 };
 
 const config: ZaptimeConfig = {
-  token: 'kPRrAtlXbgnCqi309Zjvr5Lq6RRPfYgc',
+  token: 'mlRgAVeQGqSoMjLzh5LGyyAF6xtSKKRi',
 
   compact: false,
 
-  apiBaseUrl: 'https://api.zaptime.test/',
+  // apiBaseUrl: 'https://api.zaptime.test/',
 
   // redirectAfterBookingUrl: 'https://google.com',
 
