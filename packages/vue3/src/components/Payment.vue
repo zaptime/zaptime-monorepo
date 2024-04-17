a604479fad0d65ba4ec40ccfaeeb4f0bec6d5b56
tomsq <finixit@email.cz>
2024-04-17 14:40:56 +0200

    init stripe

--- packages/vue3/src/components/Payment.vue
+++ packages/vue3/src/components/Payment.vue
@@ -1,5 +1,78 @@
 <template>
-  <div>...</div>
+  <Teleport to="head">
+    <component
+      :is="'script'"
+      src="https://js.stripe.com/v3/"
+      async
+    />
+  </Teleport>
+
+  <div
+    v-if="!result && !errors"
+    class="mx-auto mt-24 w-1/2"
+  >
+    <form
+      class="cal-mt-6"
+      @submit.prevent="handleSubmit"
+    >
+      <label
+        for="card-number"
+        class="cal-block cal-text-sm cal-font-medium cal-text-theme-500 dark:cal-text-theme-200"
+      >
+        Card Number
+      </label>
+      <div
+        id="card-number"
+        class="cal-mt-2 cal-h-[50px] cal-w-full cal-rounded-md cal-border cal-border-theme-300 cal-bg-theme-50 cal-px-5 cal-py-4 cal-text-base cal-font-medium cal-text-theme-900 cal-placeholder-theme-400 focus:cal-border-theme-400 focus:cal-outline-none focus:cal-ring-theme-500 dark:cal-border-theme-600 dark:cal-bg-theme-800 dark:cal-text-theme-100 dark:cal-placeholder-theme-500 sm:cal-text-sm"
+      >
+        <!-- A Stripe Element will be inserted here. -->
+      </div>
+
+      <div class="cal-mt-3 cal-grid cal-grid-cols-2 cal-gap-3">
+        <div>
+          <div
+            id="card-expiry"
+            class="cal-mt-2 cal-block cal-h-[50px] cal-w-full cal-rounded-md cal-border cal-border-theme-300 cal-bg-theme-50 cal-px-5 cal-py-4 cal-text-base cal-font-medium cal-text-theme-900 cal-placeholder-theme-400 focus:cal-border-theme-400 focus:cal-outline-none focus:cal-ring-theme-500 dark:cal-border-theme-600 dark:cal-bg-theme-800 dark:cal-text-theme-100 dark:cal-placeholder-theme-500 sm:cal-text-sm"
+          >
+            <!-- A Stripe Element will be inserted here. -->
+          </div>
+        </div>
+
+        <div>
+          <div
+            id="card-cvc"
+            class="cal-mt-2 cal-block cal-h-[50px] cal-w-full cal-rounded-md cal-border cal-border-theme-300 cal-bg-theme-50 cal-px-5 cal-py-4 cal-text-base cal-font-medium cal-text-theme-900 cal-placeholder-theme-400 focus:cal-border-theme-400 focus:cal-outline-none focus:cal-ring-theme-500 dark:cal-border-theme-600 dark:cal-bg-theme-800 dark:cal-text-theme-100 dark:cal-placeholder-theme-500 sm:cal-text-sm"
+          >
+            <!-- A Stripe Element will be inserted here. -->
+          </div>
+        </div>
+      </div>
+
+      <button type="submit">Pay via Stripe</button>
+    </form>
+  </div>
+
+  <div v-else>
+    <p v-if="result">Payment successful</p>
+    <p v-if="errors">{{ errors }}</p>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup>
+import { onMounted } from 'vue';
+
+import { useStripe } from '../composables/useStripe';
+
+const { initGateway, handleSubmit, result, errors } = useStripe();
+
+onMounted(async () => {
+  await initGateway();
+});
+</script>
+
+<style scoped>
+.focused {
+  border: 1px solid var(--c-zaptime-400) !important;
+  outline: none;
+}
+</style>
---
+++ packages/vue3/src/composables/useStripe.ts
@@ -0,0 +1,149 @@
+import { ref, inject, computed } from 'vue';
+import { useConfig } from '@zaptime/core';
+
+export function useStripe() {
+  const result = ref();
+  const errors = ref();
+  const paymentIntentId = ref<string>();
+  const elements = ref<stripe.elements.Elements>();
+  const stripe = ref<stripe.Stripe>();
+  const stripeCardNumber = ref<stripe.elements.Element>();
+  const stripeCardExpiry = ref<stripe.elements.Element>();
+  const stripeCardCvc = ref<stripe.elements.Element>();
+  const clientSecret = ref<string>();
+  const { config } = useConfig(inject('calendarId'));
+
+  const isDark = config.value.theme?.mode === 'dark';
+
+  const elementClasses = {
+    focus: 'focused',
+    empty: 'empty',
+    invalid: 'invalid',
+  };
+
+  const API_BASE_URL = 'https://api.zaptime.app/';
+
+  const apiBaseUrl = computed(() => {
+    return config.value.apiBaseUrl || API_BASE_URL;
+  });
+
+  const elementStyles = computed(() => {
+    return {
+      base: {
+        // @ts-expect-error values are actually required
+        color: isDark ? config.value.theme.colors[100] : config.value.theme.colors[900],
+        fontWeight: 600,
+        fontFamily:
+          '"Basier Circle", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+        fontSize: '14px',
+        fontSmoothing: 'antialiased',
+        '::placeholder': {
+          // @ts-expect-error values are actually required
+          color: isDark ? config.value.theme.colors[500] : config.value.theme.colors[400],
+        },
+        ':-webkit-autofill': {
+          // @ts-expect-error values are actually required
+          color: isDark ? config.value.theme.colors[500] : config.value.theme.colors[900],
+        },
+        ':focus': {},
+      },
+      invalid: {
+        color: '#E25950',
+        '::placeholder': {
+          color: '#FFCCA5',
+        },
+      },
+    };
+  });
+
+  async function initGateway() {
+    const res = await fetch(apiBaseUrl.value + 'api/payments', {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + config.value.token,
+      },
+    });
+
+    const { data } = await res.json();
+
+    paymentIntentId.value = data.paymentIntentId;
+    clientSecret.value = data.clientSecret;
+
+    stripe.value = Stripe(import.meta.env.VITE_STRIPE_CLIENT_KEY);
+
+    elements.value = stripe.value.elements();
+
+    stripeCardNumber.value = elements.value.create('cardNumber', {
+      style: elementStyles.value,
+      classes: elementClasses,
+    });
+
+    stripeCardExpiry.value = elements.value.create('cardExpiry', {
+      style: elementStyles.value,
+      classes: elementClasses,
+    });
+
+    stripeCardCvc.value = elements.value.create('cardCvc', {
+      style: elementStyles.value,
+      classes: elementClasses,
+    });
+
+    stripeCardNumber.value.mount('#card-number');
+    stripeCardExpiry.value.mount('#card-expiry');
+    stripeCardCvc.value.mount('#card-cvc');
+  }
+
+  async function handleSubmit() {
+    if (stripe.value === undefined || stripeCardNumber.value === undefined || clientSecret.value === undefined) {
+      return;
+    }
+
+    // const { error } = await stripe.value.confirmPayment({
+    //   elements: elements.value,
+    //   redirect: 'if_required',
+    // });
+
+    const { paymentIntent, error } = await stripe.value.confirmCardPayment(clientSecret.value, {
+      payment_method: {
+        card: stripeCardNumber.value,
+        billing_details: {
+          name: 'Jenny Rosen',
+          address: {
+            postal_code: '94103',
+          },
+        },
+      },
+    });
+
+    if (error === undefined) {
+      const response = await fetch(apiBaseUrl.value + 'api/payments/status', {
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer ' + config.value.token,
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({
+          paymentIntentId: paymentIntentId.value,
+        }),
+      });
+
+      console.log(response);
+
+      const jsonRes = await response.json();
+
+      console.log(jsonRes);
+
+      result.value = jsonRes.success;
+    } else {
+      errors.value = error.message;
+    }
+  }
+
+  return {
+    initGateway,
+    handleSubmit,
+    errors,
+    result,
+  };
+}
