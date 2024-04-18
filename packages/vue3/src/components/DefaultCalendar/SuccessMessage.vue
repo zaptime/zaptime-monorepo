7003230957edf3fefd52f80ea1439fa0f9abe19d
tomsq <finixit@email.cz>
2024-04-18 15:59:00 +0200

    neutral success message

--- packages/vue3/src/components/DefaultCalendar/SuccessMessage.vue
+++ packages/vue3/src/components/DefaultCalendar/SuccessMessage.vue
@@ -1,5 +1,6 @@
 <template>
   <div
+    v-if="selectedTimeSlot"
     class="cal-h-full cal-justify-center cal-rounded-xl cal-bg-white cal-px-0 cal-pb-20 cal-pt-24 dark:cal-bg-theme-900 sm:cal-px-6"
     :class="[config.compact ? 'cal-w-[330px] sm:cal-w-[400px]' : 'cal-w-[330px] sm:cal-w-[840px]']"
   >
@@ -18,15 +19,26 @@
       </svg>
     </div>
 
-    <h1 class="cal-mt-4 cal-text-center cal-text-2xl cal-font-semibold cal-text-theme-700 dark:cal-text-theme-300">Congratulations!</h1>
+    <h1 class="cal-mt-4 cal-text-center cal-text-2xl cal-font-semibold cal-text-theme-700 dark:cal-text-theme-300"></h1>
 
-    <h2 class="cal-mt-2 cal-text-center cal-text-2xl cal-font-medium cal-text-theme-500 dark:cal-text-theme-200">Your seat has been successfully booked.</h2>
+    <h2 class="cal-mt-2 cal-text-center cal-text-2xl cal-font-medium cal-text-theme-500 dark:cal-text-theme-200"></h2>
+
+    <h2 class="cal-mt-[24px] cal-text-center cal-font-semibold cal-text-theme-500 dark:cal-text-theme-100">
+      {{ getFormattedDayInMonth(selectedTimeSlot.start) }}
+    </h2>
+
+    <h3 class="cal-mt-2 cal-text-center cal-text-2xl cal-font-semibold cal-leading-[36px] cal-text-theme-700 dark:cal-text-theme-300">
+      {{ getFormattedTime(selectedTimeSlot.start) }} -
+      {{ getFormattedTime(selectedTimeSlot.end) }}
+    </h3>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { inject } from 'vue';
-import { useConfig } from '@zaptime/core';
+import { useConfig, useSelectedTimeSlot, useDateFormatters } from '@zaptime/core';
 
 const { config } = useConfig(inject('calendarId'));
+const { selectedTimeSlot } = useSelectedTimeSlot(inject('calendarId'));
+const { getFormattedTime, getFormattedDayInMonth } = useDateFormatters(inject('calendarId'));
 </script>
