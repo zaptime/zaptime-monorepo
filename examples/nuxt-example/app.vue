26316fa28588d1ccb144db444d80ee33d5ee26a6
tomsq <finixit@email.cz>
2024-04-22 15:15:22 +0200

    add nuxt example

---
+++ examples/nuxt-example/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <ZaptimeCalendar :config="config"></ZaptimeCalendar>
+</template>
+
+<script lang="ts" setup>
+import { ZaptimeCalendar } from '@zaptime/vue3';
+import type { ZaptimeConfig } from '@zaptime/vue3';
+
+const config: ZaptimeConfig = {
+  token: 'XxAhjGJ4vZlwjQkQtU82SeOymDUcqgnE',
+};
+</script>
+
+<style>
+@import url('/node_modules/@zaptime/vue3/dist/style.css');
+</style>
