a7a8c269fb27e256c8f57f6eb06cf5126db3563a
tomsq <finixit@email.cz>
2024-05-03 16:08:37 +0200

    fix: set select value properly

--- packages/vue3/src/components/BookingForm/components/Select.vue
+++ packages/vue3/src/components/BookingForm/components/Select.vue
@@ -7,6 +7,7 @@
       {{ label }}
     </label>
     <select
+      v-model="value"
       :name="name"
       :required="required"
       class="cal-form-select cal-mt-2 cal-block cal-w-full cal-rounded-md cal-border cal-border-theme-300 cal-bg-theme-50 cal-px-5 cal-py-3.5 cal-text-base cal-font-medium cal-text-theme-900 cal-placeholder-theme-400 focus:cal-border-theme-400 focus:cal-outline-none focus:cal-ring-theme-500 dark:cal-border-theme-600 dark:cal-bg-theme-800 dark:cal-text-theme-100 dark:cal-placeholder-theme-500 dark:focus:cal-border-theme-400 sm:cal-text-sm"
@@ -15,7 +16,6 @@
         v-for="(option, i) of options"
         :key="i"
         :value="option"
-        @click="onChange(option)"
       >
         {{ option }}
       </option>
@@ -25,13 +25,18 @@
 <script setup lang="ts">
 import type { CustomField } from '@zaptime/core';
 import { useBookingForm } from '@zaptime/core';
+import { ref, watch } from 'vue';
 
 type Props = CustomField;
 const props = defineProps<Props>();
 
+const value = ref<string>(String(props.value));
+
 const { setCustomFieldValue } = useBookingForm();
 
-function onChange(option: string) {
-  setCustomFieldValue(props.uuid, option);
-}
+watch(value, (newValue) => {
+  if (newValue === undefined) return;
+
+  setCustomFieldValue(props.uuid, newValue);
+});
 </script>
