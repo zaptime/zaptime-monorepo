7555cdaf934b67cfdab46f8761056165b3196966
tomsq <finixit@email.cz>
2024-05-06 15:54:42 +0200

    remove confirm form from colorful calendar

--- packages/vue3/src/components/ColorfulCalendar/ConfirmForm.vue
+++ packages/vue3/src/components/ColorfulCalendar/ConfirmForm.vue
@@ -22,68 +22,6 @@
         {{ getFormattedTime(selectedTimeSlot.end) }}
       </h3>
 
-      <div class="cal-mt-6 cal-max-w-[370px]">
-        <label
-          for="email"
-          class="cal-block cal-text-sm cal-font-medium cal-text-theme-700 dark:cal-text-theme-200"
-          >{{ locale?.confirmationForm?.name?.label }}</label
-        >
-        <div class="cal-mt-2">
-          <input
-            v-model="name"
-            type="text"
-            autocomplete="name"
-            name="name"
-            class="dark:cal-bg-theme-150 cal-block cal-w-full cal-rounded-md cal-border-2 cal-border-theme-300 cal-bg-theme-50 cal-px-5 cal-py-3.5 cal-text-base cal-font-medium cal-text-theme-900 cal-placeholder-theme-300 focus:cal-border-theme-500 focus:cal-outline-none focus:cal-ring-theme-500 dark:cal-border-theme-800 dark:cal-text-theme-100 dark:cal-placeholder-theme-800 sm:cal-text-sm"
-            :placeholder="locale?.confirmationForm?.name?.placeholder"
-          />
-        </div>
-      </div>
-
-      <div class="cal-mt-6 cal-max-w-[370px]">
-        <label
-          for="email"
-          class="cal-block cal-text-sm cal-font-medium cal-text-theme-700 dark:cal-text-theme-200"
-        >
-          {{ locale?.confirmationForm?.email?.label }}</label
-        >
-        <div class="cal-mt-2">
-          <input
-            v-model="email"
-            required
-            type="email"
-            name="email"
-            autocomplete="email"
-            class="dark:cal-bg-theme-150 cal-block cal-w-full cal-rounded-md cal-border-2 cal-border-theme-300 cal-bg-theme-50 cal-px-5 cal-py-3.5 cal-text-base cal-font-medium cal-text-theme-900 cal-placeholder-theme-300 focus:cal-border-theme-500 focus:cal-outline-none focus:cal-ring-theme-500 dark:cal-border-theme-800 dark:cal-text-theme-100 dark:cal-placeholder-theme-800 sm:cal-text-sm"
-            :placeholder="locale?.confirmationForm?.email?.placeholder"
-          />
-        </div>
-      </div>
-
-      <div
-        v-if="selectedTimeSlot.seats > 1"
-        class="cal-mt-6 cal-max-w-[370px]"
-      >
-        <label
-          for="email"
-          class="cal-block cal-text-sm cal-font-medium cal-text-theme-700 dark:cal-text-theme-200"
-        >
-          {{ locale?.confirmationForm?.seats?.label }}</label
-        >
-        <div class="cal-mt-2">
-          <input
-            v-model="seats"
-            type="number"
-            :min="1"
-            :max="selectedTimeSlot.seats"
-            name="seats"
-            autocomplete="off"
-            class="dark:cal-bg-theme-150 cal-block cal-w-full cal-rounded-md cal-border-2 cal-border-theme-800 cal-bg-theme-50 cal-px-5 cal-py-3.5 cal-text-base cal-font-medium cal-text-theme-100 cal-placeholder-theme-800 focus:cal-border-theme-500 focus:cal-outline-none focus:cal-ring-theme-500 sm:cal-text-sm"
-            :placeholder="locale?.confirmationForm?.seats?.placeholder"
-          />
-        </div>
-      </div>
-
       <div class="cal-mt-[32px] cal-flex cal-justify-between">
         <SecondaryButton @click="$emit('goBack')">
           {{ locale?.confirmationForm?.buttons?.goBack }}
