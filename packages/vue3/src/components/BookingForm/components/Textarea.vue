4d655b48d072ca3da3fe21e72a996570b4e674b9
tomsq <finixit@email.cz>
2024-05-17 10:34:59 +0200

    fix: textera input handling

--- packages/vue3/src/components/BookingForm/components/Textarea.vue
+++ packages/vue3/src/components/BookingForm/components/Textarea.vue
@@ -12,7 +12,7 @@
         :required="required"
         class="cal-block cal-w-full cal-rounded-md cal-border cal-border-theme-300 cal-bg-theme-50 cal-px-5 cal-py-3.5 cal-text-base cal-font-medium cal-text-theme-900 cal-placeholder-theme-400 focus:cal-border-theme-400 focus:cal-outline-none focus:cal-ring-theme-500 dark:cal-border-theme-600 dark:cal-bg-theme-800 dark:cal-text-theme-100 dark:cal-placeholder-theme-500 dark:focus:cal-border-theme-400 sm:cal-text-sm"
         :placeholder="placeholder"
-        @change="onChange"
+        @input="onInput"
       ></textarea>
     </div>
   </div>
@@ -27,8 +27,8 @@ const props = defineProps<Props>();
 
 const { setCustomFieldValue } = useBookingForm();
 
-function onChange(e: Event) {
-  if (e.target instanceof HTMLInputElement) {
+function onInput(e: Event) {
+  if (e.target instanceof HTMLTextAreaElement) {
     setCustomFieldValue(props.uuid, e.target.value);
   }
 }
