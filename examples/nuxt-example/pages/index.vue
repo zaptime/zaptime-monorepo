5c1486375a777b2dae9d6f2807e8679ebdab8aa2
tomsq <finixit@email.cz>
2025-03-21 22:25:01 +0100

    upgrade nuxt config

--- examples/nuxt-example/pages/index.vue
+++ examples/nuxt-example/pages/index.vue
@@ -2,45 +2,39 @@
   <NuxtLink to="/page2">Go to page 2</NuxtLink>
 
   {{ calendarId1 }}
-  <ZaptimeCalendar
-    :calendar-id="calendarId1"
-    :config="config"
-  ></ZaptimeCalendar>
+  <ZaptimeCalendar :calendar-id="calendarId1" :config="config"></ZaptimeCalendar>
 
-  <ZaptimeCalendar
-    :calendar-id="calendarId2"
-    :config="config2"
-  ></ZaptimeCalendar>
+  <ZaptimeCalendar :calendar-id="calendarId2" :config="config2"></ZaptimeCalendar>
 </template>
 
 <script lang="ts" setup>
-import { ZaptimeCalendar, reserve, confirm, book } from '@zaptime/vue3';
-import type { ZaptimeConfig } from '@zaptime/vue3';
+import { ZaptimeCalendar, reserve, confirm, book } from "@zaptime/vue3";
+import type { ZaptimeConfig } from "@zaptime/vue3";
 
-const calendarId1 = 'this-is-my-calendar-id';
+const calendarId1 = "this-is-my-calendar-id";
 const config: ZaptimeConfig = {
-  token: 'bINIbfBAOI8YOe5wYfJcwbx58l1Clapt',
-  apiBaseUrl: 'https://api.zaptime.test/',
-  externalBooking: true,
-  profileImage: '',
+  token: "qM0IbQM2g0Mgk9ftP3yQgFVNbixazEPp",
+  // apiBaseUrl: "https://api.zaptime.test/",
+  // externalBooking: true,
+  profileImage: "",
   locale: {
     texts: {
-      introduction: '',
+      introduction: "",
     },
   },
 
   hideLocation: true,
 };
 
-const calendarId2 = 'this-is-my-calendar-id-2';
+const calendarId2 = "this-is-my-calendar-id-2";
 const config2: ZaptimeConfig = {
-  token: 'mjkzfnQrQzkFSNICYUkbhi9F8trZGKVe',
-  apiBaseUrl: 'https://api.zaptime.test/',
+  token: "mjkzfnQrQzkFSNICYUkbhi9F8trZGKVe",
+  apiBaseUrl: "https://api.zaptime.test/",
   externalBooking: true,
-  profileImage: '',
+  profileImage: "",
   locale: {
     texts: {
-      introduction: '',
+      introduction: "",
     },
   },
 
