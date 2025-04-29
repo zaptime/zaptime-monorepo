222dea025119e15c70dacd2b2cd334c92f469dd5
tomsq <finixit@email.cz>
2025-04-29 23:16:35 +0200

    wip

--- packages/vue3/src/components/DefaultCalendar/DaysGrid.vue
+++ packages/vue3/src/components/DefaultCalendar/DaysGrid.vue
@@ -1,18 +1,58 @@
 <template>
   <div>
-    <div v-if="!state.loading" class="cal-grid cal-grid-cols-7 cal-gap-y-1">
-      <div v-for="(day, i) in state.days" :key="i" class="cal-mt-2 cal-flex cal-justify-center">
+    <div
+      v-if="!state.loading"
+      class="cal-grid cal-relative cal-grid-cols-7 cal-gap-y-1"
+    >
+      <div
+        v-if="!state.monthHasTimeSlots"
+        class="md:cal-hidden cal-absolute cal-inset-0"
+      >
+        <div
+          class="dark:cal-text-theme cal-mt-1.5 cal-flex cal-flex-col cal-h-full cal-items-center cal-justify-center cal-rounded-2xl cal-px-5 cal-backdrop-blur-[2px] cal-bg-[#ffffff8c] cal-border cal-border-white/30 dark:cal-bg-gray-800/20 dark:cal-border-white/10"
+        >
+          <p
+            v-if="config.locale && config.locale.texts?.noTimeSlotAvailable"
+            class="dark:cal-text-theme-theme cal-text-sm cal-text-theme-700 cal-text-center"
+            v-html="config.locale.texts?.noTimeSlotAvailable"
+          ></p>
+
+          <button
+            class="cal-mt-4 cal-rounded-lg cal-bg-accent-base cal-pl-4 cal-pr-2 cal-py-2 cal-text-sm cal-font-medium cal-text-white cal-shadow-sm hover:cal-bg-accent-dark focus:cal-outline-none focus:cal-ring-2 focus:cal-ring-theme-500 focus:cal-ring-offset-2 dark:cal-bg-theme-600 dark:hover:cal-bg-theme-700 dark:focus:cal-ring-theme-400 cal-transition-all cal-duration-75 cal-ease-out cal-group"
+            @click="next"
+          >
+            <span class="cal-flex cal-items-center cal-justify-center">
+              {{ config.locale?.texts?.showNextMonth }}
+              <ChevronRightIcon
+                class="cal-ml-1 group-hover:cal-translate-x-1 cal-transition-all cal-duration-75 cal-ease-out cal-h-6 cal-w-6"
+              />
+            </span>
+          </button>
+        </div>
+      </div>
+
+      <div
+        v-for="(day, i) in state.days"
+        :key="i"
+        class="cal-mt-2 cal-flex cal-justify-center"
+      >
         <div class="cal-flex cal-items-center cal-justify-center">
           <button
-            :disabled="!dayHasTimeSlot(day) || (day.isPast && !day.isCurrentMonth)"
+            :disabled="
+              !dayHasTimeSlot(day) || (day.isPast && !day.isCurrentMonth)
+            "
             :class="{
               'cal-bg-theme-100 cal-font-medium  cal-text-theme-700 cal-ring-theme-200 focus:cal-outline-none focus:cal-ring-2 dark:cal-bg-theme-800 dark:cal-text-theme-100 dark:hover:cal-text-theme-300':
                 dayHasTimeSlot(day) && !day.isPast && !isSelectedDay(day),
-              'cal-cursor-default cal-text-theme-500 dark:cal-text-theme-300': (day.isPast && !day.isCurrentMonth) || (!dayHasTimeSlot(day) && day.isCurrentMonth),
-              'cal-bg-accent-base cal-font-semibold cal-text-white  dark:cal-bg-accent-base dark:cal-text-white': isSelectedDay(day),
+              'cal-cursor-default cal-text-theme-500 dark:cal-text-theme-300':
+                (day.isPast && !day.isCurrentMonth) ||
+                (!dayHasTimeSlot(day) && day.isCurrentMonth),
+              'cal-bg-accent-base cal-font-semibold cal-text-white  dark:cal-bg-accent-base dark:cal-text-white':
+                isSelectedDay(day),
               'cal-transition-color cal-bg-theme-100 cal-text-theme-700 cal-duration-100 hover:cal-bg-theme-200 hover:cal-text-accent-base hover:dark:cal-bg-theme-700 hover:dark:cal-text-accent-base':
                 !isSelectedDay(day) && dayHasTimeSlot(day) && !day.isPast,
-              'cal-pointer-events-none cal-text-theme-500 dark:cal-text-theme-300': day.isPast,
+              'cal-pointer-events-none cal-text-theme-500 dark:cal-text-theme-300':
+                day.isPast,
             }"
             class="cal-h-10 cal-w-10 cal-rounded-md cal-text-sm"
             @click.prevent="dayClickedLocalProxy(day)"
@@ -31,15 +71,20 @@
 
 <script setup lang="ts">
 import { inject } from "vue";
-import { useCalendar } from "@zaptime/core";
+import { useCalendar, useConfig } from "@zaptime/core";
 import { Day } from "@zaptime/core";
+import { ChevronRightIcon } from "@heroicons/vue/20/solid";
 
 import useCalendarViewState from "../../composables/useCalendarViewState";
 import DaysGridLoadingSkeleton from "./DaysGridLoadingSkeleton.vue";
 
 const { setCalendarView } = useCalendarViewState(inject("calendarId"));
 
-const { dayHasTimeSlot, isSelectedDay, dayClicked, state } = useCalendar(inject("calendarId"));
+const { config } = useConfig(inject("calendarId"));
+
+const { dayHasTimeSlot, isSelectedDay, dayClicked, state, next } = useCalendar(
+  inject("calendarId"),
+);
 
 const dayClickedLocalProxy = (day: Day) => {
   dayClicked(day);
--- packages/vue3/src/components/DefaultCalendar/TimeSelection.vue
+++ packages/vue3/src/components/DefaultCalendar/TimeSelection.vue
@@ -1,9 +1,18 @@
 <template>
   <div
     class="cal-mx-auto cal-h-full cal-min-h-[524px] cal-bg-white cal-px-[20px] dark:cal-bg-theme-900 sm:cal-mx-0"
-    :class="[config.compact ? 'cal-w-[330px] cal-rounded-xl sm:cal-w-[400px]' : 'cal-w-[330px] cal-rounded-r-xl sm:cal-w-[440px]', config.profileImage ? 'cal-pt-4' : 'cal-pt-10']"
+    :class="[
+      config.compact
+        ? 'cal-w-[330px] cal-rounded-xl sm:cal-w-[400px]'
+        : 'cal-w-[330px] cal-rounded-r-xl sm:cal-w-[440px]',
+      config.profileImage ? 'cal-pt-4' : 'cal-pt-10',
+    ]"
   >
-    <PrimaryButton v-if="config.compact === true" class="cal-mb-8 cal-mt-5" @click="goPickingDates">
+    <PrimaryButton
+      v-if="config.compact === true"
+      class="cal-mb-8 cal-mt-5"
+      @click="goPickingDates"
+    >
       {{ config.locale?.confirmationForm?.buttons?.goBack }}
     </PrimaryButton>
     <div v-if="!state.loading" class="cal-h-full">
@@ -16,27 +25,60 @@
         leave-to-class="cal-opacity-0 cal-translate-x-[1px]"
         mode="out-in"
       >
-        <div v-if="state.timeSlots && state.timeSlots.length > 0 && !state.loading" class="cal-flex cal-h-full cal-flex-col cal-justify-between">
+        <div
+          v-if="state.timeSlots && state.timeSlots.length > 0 && !state.loading"
+          class="cal-flex cal-h-full cal-flex-col cal-justify-between"
+        >
           <div>
-            <div :class="[(config.locale?.texts?.introduction || config.profileImage) && !reservation && !config.compact ? 'cal-pt-[54px]' : 'cal-pt-0']">
-              <p class="cal-text-2xl cal-font-semibold cal-tracking-tighter cal-text-theme-600 dark:cal-text-theme-300">
+            <div
+              :class="[
+                (config.locale?.texts?.introduction || config.profileImage) &&
+                !reservation &&
+                !config.compact
+                  ? 'cal-pt-[54px]'
+                  : 'cal-pt-0',
+              ]"
+            >
+              <p
+                class="cal-text-2xl cal-font-semibold cal-tracking-tighter cal-text-theme-600 dark:cal-text-theme-300"
+              >
                 {{ getFormattedDayInMonth(state.timeSlots[0].start) }}
               </p>
             </div>
             <div class="cal-pb-0.5 cal-pt-6">
-              <p v-if="config.locale && config.locale.texts?.choosePreferredTime" class="cal-text-base cal-font-medium cal-tracking-tighter cal-text-theme-600 dark:cal-text-theme-200">
+              <p
+                v-if="config.locale && config.locale.texts?.choosePreferredTime"
+                class="cal-text-base cal-font-medium cal-tracking-tighter cal-text-theme-600 dark:cal-text-theme-200"
+              >
                 {{ config.locale.texts?.choosePreferredTime }}
               </p>
             </div>
 
-            <div class="cal-mt-2 cal-overflow-y-auto cal-px-1" :class="[config.locale?.hideTimePreferences ? 'cal-h-[375px]' : 'cal-h-[345px]']">
-              <div v-for="(timeSlot, i) in state.timeSlots" :key="i" class="w-full">
-                <ConfirmationButton :time-slot="timeSlot" @select-time-slot="select(timeSlot)"></ConfirmationButton>
+            <div
+              class="cal-mt-2 cal-overflow-y-auto cal-px-1"
+              :class="[
+                config.locale?.hideTimePreferences
+                  ? 'cal-h-[375px]'
+                  : 'cal-h-[345px]',
+              ]"
+            >
+              <div
+                v-for="(timeSlot, i) in state.timeSlots"
+                :key="i"
+                class="w-full"
+              >
+                <ConfirmationButton
+                  :time-slot="timeSlot"
+                  @select-time-slot="select(timeSlot)"
+                ></ConfirmationButton>
               </div>
             </div>
           </div>
 
-          <div v-if="!config.locale?.hideTimePreferences" class="cal-mt-5 cal-flex cal-items-center cal-space-x-2 cal-pb-4 sm:cal-mt-0">
+          <div
+            v-if="!config.locale?.hideTimePreferences"
+            class="cal-mt-5 cal-flex cal-items-center cal-space-x-2 cal-pb-4 sm:cal-mt-0"
+          >
             <TimeZonePicker class="cal-px-1"></TimeZonePicker>
             <Switch v-model="hourCycleSwitchValue" class="cal-py-3">
               <template #before> am/pm </template>
@@ -46,17 +88,55 @@
         </div>
 
         <div v-else class="cal-w-full">
-          <div v-if="!state.monthHasTimeSlots" class="cal-flex cal-h-full cal-flex-col cal-items-center cal-justify-center">
-            <div class="cal-mt-24 cal-flex cal-h-full cal-items-center cal-justify-center">
-              <div class="dark:cal-text-themered-50 cal-flex cal-h-40 cal-w-40 cal-items-center cal-justify-center cal-rounded-2xl cal-px-5">
-                <p v-if="config.locale && config.locale.texts?.noTimeSlotAvailable" class="dark:cal-text-theme-theme cal-text-center" v-html="config.locale.texts?.noTimeSlotAvailable"></p>
+          <div
+            v-if="!state.monthHasTimeSlots"
+            class="cal-flex cal-h-full cal-flex-col cal-items-center cal-justify-center"
+          >
+            <div
+              class="cal-mt-48 cal-flex cal-h-full cal-items-center cal-justify-center"
+            >
+              <div
+                class="dark:cal-text-theme cal-flex cal-flex-col cal-h-40 cal-items-center cal-justify-center cal-rounded-2xl cal-px-5"
+              >
+                <p
+                  v-if="
+                    config.locale && config.locale.texts?.noTimeSlotAvailable
+                  "
+                  class="dark:cal-text-theme-theme cal-text-sm cal-text-theme-700 cal-text-center"
+                  v-html="config.locale.texts?.noTimeSlotAvailable"
+                ></p>
+
+                <button
+                  class="cal-mt-4 cal-rounded-lg cal-bg-accent-dark cal-pl-4 cal-pr-2 cal-py-2 cal-text-sm cal-font-medium cal-text-white cal-shadow-sm hover:cal-bg-accent-base focus:cal-outline-none focus:cal-ring-2 focus:cal-ring-theme-500 focus:cal-ring-offset-2 dark:cal-bg-theme-600 dark:hover:cal-bg-theme-700 dark:focus:cal-ring-theme-400 cal-transition-all cal-duration-75 cal-ease-out cal-group"
+                  @click="next"
+                >
+                  <span class="cal-flex cal-items-center cal-justify-center">
+                    Zobrazit další měsíc
+                    <ChevronRightIcon
+                      class="cal-ml-1 group-hover:cal-translate-x-1 cal-transition-all cal-duration-75 cal-ease-out cal-h-6 cal-w-6"
+                    />
+                  </span>
+                </button>
               </div>
             </div>
           </div>
-          <div v-else class="cal-flex cal-flex-col cal-items-center cal-justify-center">
-            <div class="cal-mt-48 cal-flex cal-h-full cal-items-center cal-justify-center">
-              <div class="cal-flex cal-w-40 cal-flex-col cal-items-center cal-justify-center cal-rounded-2xl cal-px-7 cal-py-5 cal-text-theme-700 dark:cal-text-theme-200">
-                <svg width="54" height="60" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <div
+            v-else
+            class="cal-flex cal-flex-col cal-items-center cal-justify-center"
+          >
+            <div
+              class="cal-mt-48 cal-flex cal-h-full cal-items-center cal-justify-center"
+            >
+              <div
+                class="cal-flex cal-w-40 cal-flex-col cal-items-center cal-justify-center cal-rounded-2xl cal-px-7 cal-py-5 cal-text-theme-700 dark:cal-text-theme-200"
+              >
+                <svg
+                  width="54"
+                  height="60"
+                  viewBox="0 0 18 20"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
                   <path
                     d="M16.5 9.58332V7.33332C16.5 5.93319 16.5 5.23313 16.2275 4.69835C15.9878 4.22794 15.6054 3.84549 15.135 3.60581C14.6002 3.33332 13.9001 3.33332 12.5 3.33332H5.5C4.09987 3.33332 3.3998 3.33332 2.86502 3.60581C2.39462 3.84549 2.01217 4.22794 1.77248 4.69835C1.5 5.23313 1.5 5.93319 1.5 7.33332V14.3333C1.5 15.7335 1.5 16.4335 1.77248 16.9683C2.01217 17.4387 2.39462 17.8212 2.86502 18.0608C3.3998 18.3333 4.09987 18.3333 5.5 18.3333H9.41667M16.5 8.33332H1.5M12.3333 1.66666V4.99999M5.66667 1.66666V4.99999M14 17.5V12.5M11.5 15H16.5"
                     stroke="currentColor"
@@ -66,7 +146,11 @@
                   />
                 </svg>
 
-                <p v-if="config.locale && config.locale.texts?.chooseDate" class="cal-mt-2 cal-w-40 cal-text-center cal-text-theme-900 dark:cal-text-theme-200" v-html="config.locale.texts?.chooseDate"></p>
+                <p
+                  v-if="config.locale && config.locale.texts?.chooseDate"
+                  class="cal-mt-2 cal-w-40 cal-text-center cal-text-theme-900 dark:cal-text-theme-200"
+                  v-html="config.locale.texts?.chooseDate"
+                ></p>
               </div>
             </div>
           </div>
@@ -77,7 +161,13 @@
 </template>
 
 <script setup lang="ts">
-import { useCalendar, useConfig, useHourCycle, useDateFormatters, useReservationReschedule } from "@zaptime/core";
+import {
+  useCalendar,
+  useConfig,
+  useHourCycle,
+  useDateFormatters,
+  useReservationReschedule,
+} from "@zaptime/core";
 import useCalendarViewState from "../../composables/useCalendarViewState";
 import PrimaryButton from "../atomic/PrimaryButton.vue";
 import { inject, computed } from "vue";
@@ -86,17 +176,13 @@ import ConfirmationButton from "./TimeConfirmationButton.vue";
 import TimeZonePicker from "../atomic/TimeZonePicker.vue";
 import Switch from "../atomic/Switch.vue";
 import { getAnalytics } from "../../analytics";
+import { ChevronRightIcon } from "@heroicons/vue/20/solid";
 
 const { hourCycle } = useHourCycle();
-
 const { setView, setCalendarView } = useCalendarViewState(inject("calendarId"));
-
-const { selectTimeSlot, state } = useCalendar(inject("calendarId"));
-
+const { selectTimeSlot, state, next } = useCalendar(inject("calendarId"));
 const { config } = useConfig(inject("calendarId"));
-
 const { getFormattedDayInMonth } = useDateFormatters();
-
 const { reservation } = useReservationReschedule(inject("calendarId"));
 
 const hourCycleSwitchValue = computed({
